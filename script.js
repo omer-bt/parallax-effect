ade90c9e84c5c99e5df62a3b3b2f57c9641e71f2
omer-bt <omerbt1@hotmail.com>
2022-04-18 16:13:59 +0200

    few changes

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
 timeline
-  .fromTo(".pic", { y: -50 }, { y: 0, duration: 10 }, "-=10")
+  .fromTo(".pic", { y: -50 }, { y: 0, duration: 12 }, "-=10")
   .to(".content", 10, { top: "0%" }, "-=10")
-  .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 3 })
-  .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 2 });
+  .fromTo(".water", { opacity: 0 }, { opacity: 1, duration: 2 })
+  .fromTo(".bushes", { y: -100 }, { y: 0, duration: 10 }, "-=10")
+  .to(".content", 10, { top: "0%" }, "-=10")
+  
+  
 
 let scene = new ScrollMagic.Scene({
   triggerElement: "section",
